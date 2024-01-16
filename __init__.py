79ef97d78883b9fd342f5a2859b79ce6d5260e16
rmadeye <rafmadaj@gmail.com>
2024-01-16 11:38:07 +0100

    upd

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 
-from .pymol.draw import PyMolColor
-from . import parse
-from . import io
-from . import feature
-from .plot.tools import tensor_to_3d_position
+# from .pymol.draw import PyMolColor
+# from . import parse
+# from . import io
+# from . import feature
+# from .plot.tools import tensor_to_3d_position
 
 
