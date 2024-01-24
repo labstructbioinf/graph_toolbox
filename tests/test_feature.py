e19179964cacaf0e5d048493acd8d461fa6073ca
Kamil Kamiński <k.kaminski@cent.uw.edu.pl>
2024-01-24 14:29:40 +0100

    update

--- tests/test_feature.py
+++ tests/test_feature.py
@@ -1,7 +1,7 @@
 import pytest
 
 from feature import read_struct
-
+from feature import GraphData
 
 @pytest.mark.parametrize("pdb", [
     "tests/data/3sxw.pdb.gz",
@@ -10,3 +10,11 @@ from feature import read_struct
 def test_calc(pdb):
 
     u, v, feats, r = read_struct(pdb)
+
+@pytest.mark.parametrize("pdb", [
+    "tests/data/3sxw.pdb.gz",
+    "tests/data/6iii.pdb"
+])
+def test_graphobject(pdb):
+
+    data = GraphData.from_pdb(pdb)
